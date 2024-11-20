c7197f124b546bc0cf3e22859aadbfe265d5a835
ammrshmbng <ammarmare01@gmail.com>
2024-11-20 17:30:14 +0700

    add get endpoint

--- back-end/server.js
+++ back-end/server.js
@@ -6,6 +6,16 @@ import Product from "./models/product.model.js";
 const app = express();
 app.use(express.json()); // allows us to accept JSON data in the req.body
 
+app.get("/api/products", async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ success: true, data: products });
+    } catch (error) {
+        console.log("error in fetching products:", error.message);
+        res.status(500).json({ success: false, message: "Server Error" });
+    }
+});
+
 app.post("/api/products", async (req, res) => {
     const product = req.body; // user will send this data
   
