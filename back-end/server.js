d1ae9390b6054f35f820543f990218a3b2830fbc
ammrshmbng <ammarmare01@gmail.com>
2024-11-20 17:17:04 +0700

    add delete endpoint

--- back-end/server.js
+++ back-end/server.js
@@ -24,6 +24,15 @@ app.post("/api/products", async (req, res) => {
     }
   });
 
+  app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted" });
+      } catch (error) {
+        res.status(404).json({ success: false, message: "Product not found" });
+      }
+  });
 
 app.listen(5000, () => {
   connectDB();
