91718a0e6d61a6fae6f4113b732730657f2bfa63
ammrshmbng <ammarmare01@gmail.com>
2024-11-20 20:43:08 +0700

    add update endpoint

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
+import mongoose from "mongoose";
 
 
 const app = express();
@@ -30,6 +31,22 @@ app.post("/api/products", async (req, res) => {
       res.status(201).json({ success: true, data: newProduct });
     } catch (error) {
       console.error("Error in Create product:", error.message);
+      res.status(500).json({ success: false, message: "Server Error" });
+    }
+  });
+
+  app.put("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    const product = req.body;
+  
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(404).json({ success: false, message: "Invalid Product Id" });
+    }
+  
+    try {
+      const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
+      res.status(200).json({ success: true, data: updatedProduct });
+    } catch (error) {
       res.status(500).json({ success: false, message: "Server Error" });
     }
   });
