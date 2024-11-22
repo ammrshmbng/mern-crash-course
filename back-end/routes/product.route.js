aea1507208ea8f0458c2cf2d74b98349b73b2fa8
ammrshmbng <ammarmare01@gmail.com>
2024-11-22 16:31:39 +0700

    add update and delete controller

--- back-end/routes/product.route.js
+++ back-end/routes/product.route.js
@@ -2,36 +2,14 @@ import express from "express";
 const router = express.Router();
 import Product from "../models/product.model.js";
 import mongoose from "mongoose";
-import { createProduct, getProducts } from "../controllers/product.controller.js";
+import { createProduct, deleteProduct, getProducts, updateProduct } from "../controllers/product.controller.js";
 
 router.get("/", getProducts);
 
 router.post("/", createProduct);
 
-router.put("/:id", async (req, res) => {
-    const { id } = req.params;
-    const product = req.body;
-  
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(404).json({ success: false, message: "Invalid Product Id" });
-    }
-  
-    try {
-      const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true });
-      res.status(200).json({ success: true, data: updatedProduct });
-    } catch (error) {
-      res.status(500).json({ success: false, message: "Server Error" });
-    }
-  });
+router.put("/:id", updateProduct);
 
-router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: "Product deleted" });
-      } catch (error) {
-        res.status(404).json({ success: false, message: "Product not found" });
-      }
-  });
+router.delete("/:id", deleteProduct);
 
 export default router;
