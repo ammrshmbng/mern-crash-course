96a163f9313091352ca9708c576a1ce61b94bd58
ammrshmbng <ammarmare01@gmail.com>
2024-11-22 16:30:17 +0700

    add create product controller

--- back-end/routes/product.route.js
+++ back-end/routes/product.route.js
@@ -2,27 +2,11 @@ import express from "express";
 const router = express.Router();
 import Product from "../models/product.model.js";
 import mongoose from "mongoose";
-import { getProducts } from "../controllers/product.controller.js";
+import { createProduct, getProducts } from "../controllers/product.controller.js";
 
 router.get("/", getProducts);
 
-router.post("/", async (req, res) => {
-    const product = req.body; // user will send this data
-  
-    if (!product.name || !product.price || !product.image) {
-      return res.status(400).json({ success: false, message: "Please provide all fields" });
-    }
-  
-    const newProduct = new Product(product);
-  
-    try {
-      await newProduct.save();
-      res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-      console.error("Error in Create product:", error.message);
-      res.status(500).json({ success: false, message: "Server Error" });
-    }
-  });
+router.post("/", createProduct);
 
 router.put("/:id", async (req, res) => {
     const { id } = req.params;
