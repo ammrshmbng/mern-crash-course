187a889183fc0fbc346943e4a20cde5c6bd111c9
ammrshmbng <ammarmare01@gmail.com>
2024-11-23 16:37:26 +0700

    log the data products

--- front-end/src/pages/HomePage.jsx
+++ front-end/src/pages/HomePage.jsx
@@ -1,7 +1,16 @@
 import { Container, Text, VStack } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
+import { useProductStore } from "../store/product";
+import { useEffect } from "react";
 
 const HomePage = () => {
+  const { fetchProducts, products } = useProductStore();
+
+useEffect(() => {
+    fetchProducts();
+}, [fetchProducts]);
+
+console.log("products", products);
   return (
     <Container maxW="container.xl" py={12}>
       <VStack spacing={8}>
