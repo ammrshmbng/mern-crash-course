2d2e06b1c1ddedf458d3c7aa16e1dd83c2d16577
ammrshmbng <ammarmare01@gmail.com>
2024-11-23 10:59:11 +0700

    navbar component done

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,12 +1,20 @@
-import { Button } from '@chakra-ui/react'
-import React from 'react'
+import { Box } from "@chakra-ui/react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./pages/HomePage";
+import CreatePage from "./pages/CreatePage";
 
 const App = () => {
   return (
-    <>
-      <Button>Button</Button>
-    </>
-  )
-}
+    <Box minH={"100vh"}>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/create" element={<CreatePage/>} />
 
-export default App
+      </Routes>
+    </Box>
+  );
+};
+
+export default App;
